b5ab32cd6d7e45b458c94981a21eaa655927e7fc
Thomas Heinrichs <tom.heinri@gmail.com>
2022-08-03 19:41:20 -0400

    init

---
+++ scenes/Tile.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+export var tile = 0
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AnimatedSprite.frame = tile
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Area2D_input_event(viewport, event, shape_idx):
+	print("click", self)
