9771461ba16e9b07ff29e57b2c6389036c98e92b
Thomas Heinrichs <tom.heinri@gmail.com>
2022-08-13 15:26:08 -0400

    hook up board click signals to begin building player state, begin building active commander state

--- scenes/Main.gd
+++ scenes/Main.gd
@@ -2,15 +2,35 @@ extends Node2D
 
 
 # Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var playerState = {
+	"activeTile": -1, 
+	"clickActive": false, 
+	"selectedCommander": -1, 
+	"navigation": {
+		"active": false,
+		"tileFrom": Vector2(0,0),
+		"tileTo": Vector2(0,0)
+		}
+	}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	get_node("Board/TileMap").connect("tilemapClick", self, "_on_tilemap_click_signal")
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_tilemap_click_signal(tileId, clicked_cell):
+	print("signal received ", tileId, " ", clicked_cell)
+	print("commander size: ", $Board.boardData[tileId].tile.commandersOnTile.size())
+	if $Board.boardData[tileId].tile.commandersOnTile.size() > 0:
+		playerState.clickActive = true
+		$Board/ActiveTileMarker.position = ($Board.boardData[tileId].tile.coords * 64) + Vector2($Board.tileSize / 2, $Board.tileSize / 2)
+		$Board/ActiveTileMarker.visible = true
+	elif $Board.boardData[tileId].tile.commandersOnTile.size() == 0:
+		playerState.clickActive = false
+		if $Board.has_node("activeIndicator"):
+			$Board/ActiveTileMarker.visible = false
+	#set clickActive to true
+	#set active tile to tileId
+	#make node on tile to show active
+	#check if commander on tile is owned
+	#activate movement
--- scenes/TileMap.gd
+++ scenes/TileMap.gd
@@ -1,25 +1,23 @@
 extends TileMap
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+signal tilemapClick
+var clicked_cell
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var main = get_tree().get_root()
+	
 
-func _unhandled_input(event):
+func _unhandled_input(event): #grabs tile coords from tilemap, then maths out what the id of the tile is so it can be accessed in boardData
 	if (event is InputEventMouseButton && event.pressed && Input.is_action_pressed("left_click")):
 		var camera = get_tree().get_root().get_node("Main/CameraDummy")
-		var clicked_cell = world_to_map(get_global_mouse_position())
-		#print("clicked: ", event.position, " Cam Pos: ", camera.position, " global to click: ", get_global_mouse_position())
 		var board = get_node("../../Board")
+		
+		clicked_cell = world_to_map(get_global_mouse_position())
 		var tileId = clicked_cell.x + (clicked_cell.y * board.boardSize.y)
+		
 		if(clicked_cell.x >= 0 && clicked_cell.x < board.boardSize.y && clicked_cell.y >= 0 && clicked_cell.y < board.boardSize.x):
-			print(clicked_cell, " ", tileId, " ", board.boardData[tileId])
+			#print(clicked_cell, " ", tileId, " ", board.boardData[tileId])
+			emit_signal("tilemapClick", tileId, clicked_cell)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
