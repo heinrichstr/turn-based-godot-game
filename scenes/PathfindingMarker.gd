0d22d439bdd28f19cff369c4ae6b05bffcecb7b2
Thomas Heinrichs <tom.heinri@gmail.com>
2022-08-24 13:52:24 -0400

    fix tilefinding script

--- scenes/PathfindingMarker.gd
+++ scenes/PathfindingMarker.gd
@@ -24,18 +24,27 @@ func tileIdByCoords(coords):
 
 
 func _draw(): #TODO: figure out how to draw from end of last array for index == 0 case
-	if drawPath[0].size() > 1:
+	if drawPath[0].size() > 0:
 		print(drawPath)
 		var arrayIndex = 0
 		var tracker = 1
 		for turnNav in drawPath:
 			for index in turnNav.size():
-				if index == 0 && arrayIndex + 1 < drawPath.size():
-					drawNavLine(turnNav[index], turnNav[index+1], Color(1* tracker,1* tracker,1* tracker,1), arrayIndex)
-				if index + 1 < turnNav.size():
-					drawNavLine(turnNav[index], turnNav[index+1], Color(1* tracker,1* tracker,1* tracker,1), -1)
-				elif index + 1 == turnNav.size() && arrayIndex + 1 < drawPath.size():
-					drawNavLine(turnNav[index], drawPath[arrayIndex + 1][0], Color(1* tracker,1* tracker,1* tracker,1), -1)
+				#write to circle drawing function if start of new turn movement
+				if index == 0:
+					turnPoints.append([(turnNav[index] * PlayerState.boardNode.tileSize) + (Vector2(1,1) * (PlayerState.boardNode.tileSize/2)), arrayIndex])
+				
+				#draw a line from current to next as long as there is a next
+				if turnNav.size() > index + 1:
+					print("turnNav: ", turnNav, " size ", turnNav.size())
+					print("Drawing from: ", turnNav[index], " at index ", index)
+					print("Drawing to: ", turnNav[index + 1])
+					drawNavLine(turnNav[index], turnNav[index + 1], Color(1* tracker,1* tracker,1* tracker,1), -1)
+				
+				#if next is in next array, draw to index 0 of next array (if it exists)
+				elif turnNav.size() == index + 1 && drawPath.size() > arrayIndex + 1:
+					drawNavLine(turnNav[index], drawPath[arrayIndex+1][0], Color(1* tracker,1* tracker,1* tracker,1), -1)
+			
 			arrayIndex += 1
 			tracker = tracker * .8
 	
@@ -43,7 +52,6 @@ func _draw(): #TODO: figure out how to draw from end of last array for index == 
 		for index in turnPoints.size():
 			draw_circle(turnPoints[index][0],15,Color(0,0,0))
 			draw_circle(turnPoints[index][0],13,Color(.5,.5,.5))
-			print("drawing circle for", turnPoints[index][1])
 			draw_string(font, turnPoints[index][0], str(turnPoints[index][1]))
 
 
@@ -57,40 +65,39 @@ func clear():
 
 
 func drawNavLine(fromPoint, toPoint, color, turn):
+	var tileSize = PlayerState.boardNode.tileSize
 	var tilePos = Vector2(
-		fromPoint.x * PlayerState.boardNode.tileSize + PlayerState.boardNode.tileSize / 2,
-		fromPoint.y * PlayerState.boardNode.tileSize + PlayerState.boardNode.tileSize / 2
+		fromPoint.x * tileSize + tileSize / 2,
+		fromPoint.y * tileSize + tileSize / 2
 	)
 	
 	draw_line(
-		Vector2(fromPoint.x * 64 + 32, fromPoint.y * 64 + 32), 
-		Vector2(toPoint.x * 64 + 32, toPoint.y * 64 + 32), 
+		Vector2(fromPoint.x * tileSize + tileSize / 2, fromPoint.y * tileSize + tileSize / 2), 
+		Vector2(toPoint.x * tileSize + tileSize / 2, toPoint.y * tileSize + tileSize / 2), 
 		Color(0,0,0,1), 
 		5, 
 		true
 	)
 	draw_line(
-		Vector2(fromPoint.x * 64 + 32, fromPoint.y * 64 + 32), 
-		Vector2(toPoint.x * 64 + 32, toPoint.y * 64 + 32), 
+		Vector2(fromPoint.x * tileSize + tileSize / 2, fromPoint.y * tileSize + tileSize / 2), 
+		Vector2(toPoint.x * tileSize + tileSize / 2, toPoint.y * tileSize + tileSize / 2), 
 		color, 
 		3, 
 		true
 	)
 	draw_circle(tilePos,5,Color(0,0,0))
 	draw_circle(tilePos,3,Color(1,1,1))
-	if turn >= 0:
-		turnPoints.append([tilePos, turn])
 
 
 func drawNav(points, baseMovement, movementRemaining):
-	#create an array of arrays that have path information. Each array contains a single turn of movement
+	#create an array of arrays that have path coords. Each array contains a single turn of movement
 	self.clear()
+	print("BASE NAV: ", points)
 	drawPath = [[]]
-	movementRemaining
 	navPoints = points
 	pointCosts = []
 	
-	for index in range(0, navPoints.size()):
+	for index in navPoints.size():
 		var navId = tileIdByCoords(navPoints[index])
 		var cost = PlayerState.boardNode.astar.get_point_weight_scale(navId)
 		pointCosts.append(cost)
@@ -98,18 +105,22 @@ func drawNav(points, baseMovement, movementRemaining):
 	var arrayTracker = 0
 	var costTracker = movementRemaining
 	
-	for index in range(0, navPoints.size()):
+	for index in navPoints.size():
+		#if last point in navigation
+		if index == navPoints.size() - 1:
+			drawPath[arrayTracker].append(navPoints[index])
 		
-		if (costTracker - pointCosts[index] < 0):
-			pass #break
+		#else if this move would break turn
+		elif (costTracker - pointCosts[index] < 0):
 			drawPath.append([])
 			arrayTracker += 1
 			drawPath[arrayTracker].append(navPoints[index])
-			costTracker = costTracker - pointCosts[index] + baseMovement
+			costTracker = costTracker - pointCosts[index + 1] + baseMovement
 		
+		#else add it and subject cost from turn tracking
 		else:
 			drawPath[arrayTracker].append(navPoints[index])
-			costTracker -= pointCosts[index]
+			costTracker -= pointCosts[index + 1]
 	
 	update()
 
